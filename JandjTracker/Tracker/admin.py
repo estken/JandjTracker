13ea73ba6d064dce80e3cd01d078b2b6fc65863f
David Akwuru <daakwuru@gmail.com>
2023-07-28 15:07:04 +0100

    added a custom user model and adjusted it fields, and also add a nice admin theme

--- JandjTracker/Tracker/admin.py
+++ JandjTracker/Tracker/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
-
+from .models import *
+from .user_model import ClientUser
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+class UserAdminConfig(UserAdmin):
+    # Admin can either search with email address or with username.
+    search_fields = ('email', 'username')
+    # arrange the record in descending order of date joined.
+    ordering = ('-date_joined',)
+    # change the display of the user
+    list_display = ('email','name','username', 'date_joined', 'is_staff','is_active', 'is_changed',)
+    # Arrange the fields in nice partitions.
+    fieldsets = (
+        (None, {'fields': ('email', 'username')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_changed')}),
+        ('Personal',{'fields': ('name','date_joined', 'last_login',)}),
+    )
+
+    # These are the fields we want displayed on the admin page whenever we want to create a new user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('id', 'email', 'username', 'name','password1', 'password2', 'is_staff', 'is_superuser', 'is_active', 'is_changed'),}
+        ),
+    )
+
+register = admin.site.register
+register(ClientUser, UserAdminConfig)
--- JandjTracker/Tracker/models.py
+++ JandjTracker/Tracker/models.py
@@ -1,3 +1,4 @@
 from django.db import models
+from .user_model import ClientUser
 
 # Create your models here.
